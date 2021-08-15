e68dc7dd3bbb97fa48545a2aeac43c3bab7094ac
Riyaz Padaniya <64925877+padaniyariyaz@users.noreply.github.com>
2021-08-15 02:24:55 -0400

    Update AuthController.java

--- src/main/java/com/example/vidu_clone/controllers/AuthController.java
+++ src/main/java/com/example/vidu_clone/controllers/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("https://bingewatch-ott.herokuapp.com/")
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class AuthController {
 
