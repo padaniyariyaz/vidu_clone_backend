669f9dea672d63a2dfb3dfcbc1a8196c461d805c
Riyaz Padaniya <64925877+padaniyariyaz@users.noreply.github.com>
2021-08-15 12:04:32 -0400

    Update AuthController.java

--- src/main/java/com/example/vidu_clone/controllers/AuthController.java
+++ src/main/java/com/example/vidu_clone/controllers/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class AuthController {
 
