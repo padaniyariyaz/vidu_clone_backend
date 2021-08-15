8d76e4a8c5577d11b43ede64560a2eb2e453d6b2
Riyaz Padaniya <64925877+padaniyariyaz@users.noreply.github.com>
2021-08-15 11:18:41 -0400

    Update MovieController.java

--- src/main/java/com/example/vidu_clone/controllers/MovieController.java
+++ src/main/java/com/example/vidu_clone/controllers/MovieController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://vidu-clone.herokuapp.com/")
 @RestController
 
 public class MovieController {
